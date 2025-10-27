824e2a0a1392a6d2e28f52689fcce73e0bc64e40
aleya566 <aleya.aelyana566@gmail.com>
2025-10-28 01:19:53 +0800

    Update page1_objective1.py

--- page1_objective1.py
+++ page1_objective1.py
@@ -82,8 +82,7 @@ with st.expander("🔍 View Dataset"):
 # ==============================================
 st.markdown("""
 ## 🎯 **Objective 1**
-To explore how academic stress levels and sleep patterns vary across students years of study and genders. This objective focuses on identifying patterns of **academic stress**, **sleep duration**, and **sleep quality** across different groups of students. 
-The following visualizations help highlight which groups experience higher stress levels or poorer sleep quality.
+To explore how academic stress levels and sleep patterns vary across students years of study and genders. This objective focuses on identifying patterns of **academic stress**, **sleep duration**, and **sleep quality** across different groups of students.
 """)
 
 # ==============================================
