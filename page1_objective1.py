8f40fc7988324b612e853e138d5f387a9d6efe6f
aleya566 <aleya.aelyana566@gmail.com>
2025-10-31 17:07:48 +0800

    Update page1_objective1.py

--- page1_objective1.py
+++ page1_objective1.py
@@ -124,7 +124,7 @@ fig2 = px.box(
     df,
     x='2. What is your gender?',
     y='4. On average, how many hours of sleep do you get on a typical day?',
-    color='Gender',
+    color='2. What is your gender?',
     title='Average Sleep Hours by Gender',
     color_discrete_sequence=px.colors.sequential.Sunset
 )
